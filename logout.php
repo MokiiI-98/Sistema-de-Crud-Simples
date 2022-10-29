fb25eff7fb242759ce7b5aa04a9351eb05168a25
Luan Lima <110059765+MokiiI-98@users.noreply.github.com>
2022-10-29 14:50:27 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,11 +2,9 @@
 //Não esquecer de sempre iniciar  sessão .
 session_start();
 
-// Apaga todas as variáveis da sessão
+
 $_SESSION = array();
 
-// Se é preciso matar a sessão, então os cookies de sessão também devem ser apagados.
-// Nota: Isto destruirá a sessão, e não apenas os dados!
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -15,8 +13,8 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Por último, destrói a sessão.
+
 session_destroy();
-// Redireciona para o login.
+
 header("location:login.php");
 ?>
